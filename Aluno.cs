03ed3415af1db94141f1a4a6c52760b25c18ade3
umb ams <lisboagabriel061@gmail.com>
2024-03-28 19:37:56 -0300

    C

---
+++ Aluno.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Linq;
+using System.Thrending.Tasks;
+ public class Aluno{
+ public string Name{get;set;}
+ public string School{get;set;}
+ public int Score{get;set;}
+ public int Grade{get;set;}
+ public bool Graduated{get;set;}
+ public void Study()
+ {
+
+ }
+ public void Talk()
+ {
+
+ }
+ }
